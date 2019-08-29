adee62494ab78f72650638547a1705dd614ffc27
Spiderhawk1315 <54301626+Spiderhawk1315@users.noreply.github.com>
2019-08-29 14:58:12 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Dylan Hathorn
+ * Author: Dylan Hathorn Eduardo Ponce
  * Date: 2019/08/28
  *
  * A simple hello world program in C
@@ -12,5 +12,7 @@ int main(int argc, char **argv) {
 
   printf("Dylan Hathorn\n");
   printf("Computer Science\n");
+  printf("Computer Science and Physics\n");
+  printf("Eduardo Ponce\n");
   return 0;
 }
